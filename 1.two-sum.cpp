052c4d074512c5c66365a163567f70abc653c90e
Sayandip Das <88981165+Sayandip143@users.noreply.github.com>
2022-08-14 21:44:27 +0530

    Create 1.two-sum.cpp

---
+++ 1.two-sum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+        // add to map
+        vector<int> v;
+        for(int i=0;i<nums.size();i++){
+            // if remaining requried already exists
+            if(m[target-nums[i]]!=0){
+                v.push_back(m[target-nums[i]]-1);
+                v.push_back(i);
+                break;
+            }
+            else{
+                m[nums[i]]=i+1;
+            }
+        }
+        return v;
+    }
+};
